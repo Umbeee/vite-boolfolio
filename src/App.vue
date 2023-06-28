d50c9ecffb8e959c3a4fb51f1b1de8fd7588fb55
Umbeee <umberto.nesta@hotmail.com>
2023-06-28 21:54:27 +0200

    stampato in console tutti i progetti, recuperati da axios

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <script >
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    this.getProjects()
+  },
+  methods: {
+    getProjects() {
+      axios.get('http://127.0.0.1:8000/api/projects')
+        .then(res => {
+          console.log(res.data)
+          this.projects = res.data.projects
+        })
+    }
+  }
+}
+
 
 </script>
 
 <template>
-  <h1>Ciao mondo</h1>
+  <h1>Ciao front-end</h1>
+  <div>
+  </div>
 </template>
 
 <style lang="scss">
